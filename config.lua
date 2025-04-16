685f7e5140792bc0c792b776fc4c97959606a7df
Dev Benkiran <151732143+benkirandev@users.noreply.github.com>
2025-04-16 23:35:06 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+
+------------------------------MAIN ---------
+Config.Framework = "newqb" -- newqb, oldqb, esx
+Config.BotToken = "" -- Insert bot token if Config.Service set to "discord" -- https://www.youtube.com/watch?v=-m-Z7Wav-fM
+Config.SteamApiKey = "" -- Insert steam api key if Config.Service set to "steam" -- https://steamcommunity.com/dev/apikey 
+
+Config.OpenKey = 'e'
+Config.MaxPlayer = '127'
+Config.Service = 'discord' -- steam , discord , none
+Config.ServerName = 'Codem RolePlay'
